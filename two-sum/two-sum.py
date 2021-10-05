d70b940c834c70ec55e12ebfc5f53b15636c3194
aedimoff <76131255+aedimoff@users.noreply.github.com>
2021-10-05 16:04:58 -0700

    Time: 36 ms (98.75%), Space: 14.5 MB (23.82%) - LeetHub

---
+++ two-sum/two-sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        
+        for i in range(len(nums)):
+            num = nums[i]
+            diff = target - num
+            if diff in seen:
+                return [i, seen[diff]]
+            seen[num] = i
+            
+            
